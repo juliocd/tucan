44865c8c1e3efd206013a4f354c2b8b6b02c1797
Julio Diaz <juliocm17@gmail.com>
2025-08-11 09:50:12 -0400

    Fix items per page pagination

--- backend/tucan_project/settings.py
+++ backend/tucan_project/settings.py
@@ -46,7 +46,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'landing.authentication.CookieTokenAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'landing.pagination.CustomPagination',
     'PAGE_SIZE': 10,
 }
 
